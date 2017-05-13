93259a4ed292f5729fb5203a0d98574ad10f432c
Ryan Stevens <stevens.ryan@gmail.com>
2017-05-13 12:58:14 -0700

    more changes

--- Vagrantfile
+++ Vagrantfile
@@ -9,16 +9,12 @@ Vagrant.configure(2) do |config|
     #
     #   # Customize the amount of memory on the VM:
     vb.memory = "4096"
-    vb.cpus = 2
+    vb.cpus = 2 
+    vb.customize ["setextradata", :id, "VBoxInternal2/SharedFoldersEnableSymlinksCreate/v-root", "1"]
+    vb.customize ["setextradata", :id, "VBoxInternal2/SharedFoldersEnableSymlinksCreate/www", "1"]
   end
 
   config.vm.synced_folder "../", "/home/ubuntu/projects"
-
-  #copy files so we can git push
-  config.vm.provision "file", source: "../keys/.gitconfig", destination: "/home/ubuntu/.gitconfig"
-  config.vm.provision "file", source: "../keys/id_rsa", destination: "/home/ubuntu/.ssh/id_rsa"
-  config.vm.provision "file", source: "../keys/id_rsa.pub", destination: "/home/ubuntu/.ssh/id_rsa.pub"
-
   config.vm.provision "shell", path: "./scripts/configure.sh"
   
 end
--- scripts/configure.sh
+++ scripts/configure.sh
@@ -7,7 +7,11 @@ echo 'Installing stuff'
 sudo apt install dos2unix
 sudo apt-get install python-software-properties
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install nodejs -qq
+apt-get install nodejs -qq
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
 
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
@@ -16,7 +20,18 @@ apt-cache policy docker-engine -qq
 sudo apt-get install -y docker-engine
 sudo usermod -aG docker ubuntu
 
-chmod 600 ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa.pub
+sudo apt install gcc -qq
+
+mkdir /home/ubuntu/.aws
+cp /home/ubuntu/projects/dev-environment/user-configs/.gitconfig /home/ubuntu/.gitconfig
+cp /home/ubuntu/projects/dev-environment/user-configs/id_rsa /home/ubuntu/.ssh/id_rsa
+cp /home/ubuntu/projects/dev-environment/user-configs/id_rsa.pub /home/ubuntu/.ssh/id_rsa.pub
+cp /home/ubuntu/projects/dev-environment/user-configs/aws-credentials /home/ubuntu/.aws/credentials
+sudo chmod 600 /home/ubuntu/.ssh/id_rsa
+sudo chmod 600 /home/ubuntu/.ssh/id_rsa.pub
+sudo chown ubuntu /home/ubuntu/.ssh/id_rsa
+sudo chown ubuntu /home/ubuntu/.ssh/id_rsa.pub
+
+echo 'set bell-style none' >> /home/ubuntu/.inputrc
 
 echo 'Finished running configure.sh' 
